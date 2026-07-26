2aa068cfba2dd78a2f57354ce260b6b4919cf99f
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-07-26 18:50:56 +0530

    Add files via upload

---
+++ DirectoryChecksumDeleteFinal.py
@@ -0,0 +1,73 @@
+import sys
+import os
+import hashlib
+
+def CalculateChecksum(FileName):
+    fobj = open(FileName , "rb")
+    
+    hobj = hashlib.md5()
+    Buffer = fobj.read(1024)
+    
+    while(len(Buffer)>0):
+        hobj.update(Buffer)
+        Buffer = fobj.read(1024)
+    
+    fobj.close()
+    return hobj.hexdigest()
+
+def FindDuplicate(DirectoryName):
+    Ret = False
+    
+    Ret = os.path.exists(DirectoryName)
+    
+    if Ret == False:
+        print("path is Invalid")
+        return
+    
+    Ret = os.path.isdir(DirectoryName)
+    
+    if(Ret == False):
+        print("It is not a Directory")
+        return
+    
+    Duplicate = {}
+    Unique =0
+    Same = 0
+    
+    for FolderName , SubFolder , FileName in os.walk(DirectoryName):
+        for fname in FileName:
+            fname = os.path.join(FolderName , fname)
+            
+            Checksum = CalculateChecksum(fname)
+            
+            if Checksum in Duplicate:
+               
+                Duplicate[Checksum].append(fname)
+            else:
+               
+                Duplicate[Checksum] = [fname]
+                
+    return Duplicate
+          
+def DeleteDuplicate(DirectoryName):
+    MyDict = FindDuplicate(DirectoryName)
+  
+    Result = list(filter(lambda X : len(X)>1, MyDict.values()))
+    count =0
+    TotalDeleted =0
+    
+    for value in Result:
+        for Subvalue in value:
+            count = count +1
+            if(count >1):
+                os.remove(Subvalue)
+                TotalDeleted = TotalDeleted +1
+        count = 0
+        
+    print("Total Deleted Files :", TotalDeleted)
+            
+def main():
+   DeleteDuplicate("Test")
+   
+if __name__=="__main__":
+    main()
