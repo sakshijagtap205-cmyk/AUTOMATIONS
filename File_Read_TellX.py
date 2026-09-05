768a52f1ec3e35cc5d4e2d35651a9dba912bcf21
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-09-05 20:11:20 +0530

    Add files via upload

---
+++ File_Read_TellX.py
@@ -0,0 +1,23 @@
+def main():
+    try:
+        fobj= open("Demo.txt","r")
+        print("File gets opened")
+    
+        print("File offset is :", fobj.tell())
+        Data = fobj.read(10)
+        
+        print(Data)
+        print("File offset is :", fobj.tell())   #0
+        
+        
+        Data = fobj.read(10)
+        print(Data)
+        print("File offset is :", fobj.tell())   #10
+        
+        fobj.close()
+       
+    except FileNotFoundError as fobj:
+        print("File is not present in current directory")
+    
+if __name__=="__main__":
+    main()
