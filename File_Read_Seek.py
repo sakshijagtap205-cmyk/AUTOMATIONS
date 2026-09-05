36a636a3a5e787140888fb81b1da5752f28d6451
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-09-05 20:09:18 +0530

    Add files via upload

---
+++ File_Read_Seek.py
@@ -0,0 +1,25 @@
+#seek (kuthe , kuthun)
+#kuthun : 0/1/2
+
+# 0 starting
+# 1 Current
+# 2 End
+
+
+
+def main():
+    try:
+        fobj= open("Demo.txt","r")
+        print("File gets opened")
+
+        fobj.seek(10,0)
+       
+        Data = fobj.read()
+       
+        print(Data)
+       
+    except FileNotFoundError as fobj:
+        print("File is not present in current directory")
+    
+if __name__=="__main__":
+    main()
